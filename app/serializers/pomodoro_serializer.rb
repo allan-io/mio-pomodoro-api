be6eca3d0567cef8c5e11fce266b16d7269b860f
allanolive <allan.loveforfood@gmail.com>
2019-04-14 02:26:28 -0400

    Added pomodoros attribute to user serializer
    - appeased linter in pomodoro_serializer.rb

--- app/serializers/pomodoro_serializer.rb
+++ app/serializers/pomodoro_serializer.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 class PomodoroSerializer < ActiveModel::Serializer
-  attributes :id, :work_time, :break_time, :work_percent_complete, :break_percent_complete, :active_theme
+  attributes(
+    :id,
+    :work_time,
+    :break_time,
+    :work_percent_complete,
+    :break_percent_complete,
+    :active_theme,
+    :user_id,
+    :created_at
+  )
 end
